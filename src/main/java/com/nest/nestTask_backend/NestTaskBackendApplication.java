3cb51110bc21d314104b4340f70e1c3283aff092
sufailbadushat <sufailbadushat@gamil.com>
2022-12-11 19:25:38 +0530

    fist commit_

---
+++ src/main/java/com/nest/nestTask_backend/NestTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nestTask_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestTaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestTaskBackendApplication.class, args);
+	}
+
+}
